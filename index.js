dbd8f093e61b5f8c9ed2c9bd9e207add3ee3982f
1998jeri <1998jeri@gmail.com>
2019-07-20 04:42:16 +0700

    asd

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // support json encoded bodies
 
 // routes
-const scoreRouter = require('./routes/score');
-const userRouter = require('./routes/user');
-const soalRouter = require('./routes/soal');
-const jawabanRouter = require('./routes/jawaban');
+const scoreRouter = require('/routes/score');
+const userRouter = require('/routes/user');
+const soalRouter = require('/routes/soal');
+const jawabanRouter = require('/routes/jawaban');
 
-const sequelize = require('./configs/sequelize');
+const sequelize = require('/configs/sequelize');
 
 // Models
-const Score = require('./models/score');
-const Jawaban = require('./models/jawaban');
-const User = require('./models/user');
-const Soal = require('./models/soal');
+const Score = require('/models/score');
+const Jawaban = require('/models/jawaban');
+const User = require('/models/user');
+const Soal = require('/models/soal');
 
 
 app.use('/score', scoreRouter);
