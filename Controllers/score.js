33d8efb800a4de1c9439aa0283669505337e3e58
1998jeri <1998jeri@gmail.com>
2019-07-20 11:33:25 +0700

    asd

--- Controllers/score.js
+++ Controllers/score.js
@@ -1,1 +1,29 @@
 const Score = require('../Models/score');
+
+module.exports.getNilai = (req,res) => {
+	Jawaban.findAll({
+		where: {
+			id_user: req.params.id
+		}
+	}).then(jawaban => {
+		res.json({
+			"status" : 200,
+			"message" : "success",
+			"nilai" : jawaban
+		});
+		
+	});
+}
+
+module.exports.setNilai = (req, res) =>{
+	var id_user = req.body.id;
+	var score = req.body.score;
+	Score.create({
+		id_user:    id_user,
+		score:    score
+	})
+	.then(nilai => {
+		res.json(nilai)
+	});
+
+}
--- Routes/score.js
+++ Routes/score.js
@@ -4,5 +4,7 @@ const ScoreController = require('../Controllers/score');
 
 const router = express.Router();
 
+router.post('/', ScoreController.setNilai);
+router.get('/', ScoreController.getNilai);
 
 module.exports = router;
